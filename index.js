78a937764af48f71f48a5913ecee63ac820b91da
vic778 <victoiremmanuelbarh@gmail.com>
2021-11-22 15:17:11 +0200

    Added function to save books in the array

--- index.js
+++ index.js
@@ -1,8 +1,66 @@
-const display = document.querySelector('.display');
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
-const addButton = document.querySelector('.add-button');
-
-display.innerHTML = `<p class="name"> Harry Potter </p>
-                     <p class="writer"> J. K Rowling</p>
-                     <button class="remove-button"> Remove</button>`
+const books = [];
+let retrievedBooks;
+if (localStorage.getItem('books') === null) {
+  retrievedBooks = localStorage.setItem('books', JSON.stringify(books));
+} else {
+  retrievedBooks = JSON.parse(localStorage.getItem('books'));
+}
+const displayBooks = () => {
+  retrievedBooks = JSON.parse(localStorage.getItem('books'));
+  retrievedBooks.forEach((book) => {
+    const parentDiv = document.querySelector('.books');
+    const div = document.createElement('div');
+    div.classList.add('book');
+    div.innerHTML = `
+    <h5>${book.title}</h5>  
+    <p>${book.author}</p>  
+    <button class="remove">remove</button>
+    <hr>
+  `;
+    parentDiv.appendChild(div);
+  });
+};
+const addButton = document.querySelector('#add-button');
+const addBook = (e) => {
+  retrievedBooks = JSON.parse(localStorage.getItem('books'));
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const book = {
+    title,
+    author,
+  };
+  book.id = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  retrievedBooks.push(book);
+  localStorage.setItem('books', JSON.stringify(retrievedBooks));
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+  const parentDiv = document.querySelector('.books');
+  const div = document.createElement('div');
+  div.classList.add('book');
+  div.dataset.id = book.id;
+  div.innerHTML = `
+    <h5>${book.title}</h5>  
+    <p>${book.author}</p>  
+    <button class="remove">remove</button>
+    <hr>
+  `;
+  parentDiv.appendChild(div);
+};
+const removeBook = (elem) => {
+  if (elem.classList.contains('remove')) {
+    elem.parentElement.remove();
+  }
+};
+document.addEventListener('DOMContentLoaded', displayBooks);
+addButton.addEventListener('click', addBook);
+document.querySelector('.books').addEventListener('click', (e) => {
+  removeBook(e.target);
+  const newBooks = JSON.parse(localStorage.getItem('books'));
+  newBooks.forEach((book, index) => {
+    if (book.author === e.target.previousElementSibling.textContent) {
+      newBooks.splice(index, 1);
+    }
+  });
+  localStorage.setItem('books', JSON.stringify(newBooks));
+});
