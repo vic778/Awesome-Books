756af478a00566896ddfbfb0f44da3bdf735f230
Vagyasri <vagyasri2011@gmail.com>
2021-11-22 10:45:31 +0100

    Add display Values in DoM

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const display = document.querySelector('.display');
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const addButton = document.querySelector('.add-button');
+
+display.innerHTML = `<p class="name"> Harry Potter </p>
+                     <p class="writer"> J. K Rowling</p>
+                     <button class="remove-button"> Remove</button>`
