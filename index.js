c9571532cc3634f8535b368d4d3c26ac3ce68c35
Vagyasri <vagyasri2011@gmail.com>
2021-11-22 10:37:42 +0100

    Link JS File

---
+++ index.js
@@ -0,0 +1,4 @@
+const display = document.querySelector('.display');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const addButton = document.querySelector('.add-button');
