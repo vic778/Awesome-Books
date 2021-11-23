4ebd1cee5bd9a00d2780afb5271a929418c63929
Vagyasri <vagyasri2011@gmail.com>
2021-11-23 14:51:19 +0100

    Add Elements

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let books = document.querySelectorAll('.books');
+let title = document.querySelectorAll('.title');
+let author = document.querySelectorAll('.author');
+let addButton = document.querySelectorAll('.add-button');
+
 class Book {
    constructor(title, author) {
     this.title = title;
